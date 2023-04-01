074552a0547833a1a1872f055585558a62357cdf
kny2383 <kny2383@gmail.com>
2023-04-02 04:33:05 +0900

    Typical screen configuration of up, down, and center space

---
+++ Doit_2/app/src/main/java/com/example/doit2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.doit2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // 2021041059 김나영
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
