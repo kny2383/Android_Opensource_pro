fe5dbbf98e302ef9962e2d453bada9a7e6a857d3
kny2383 <kny2383@gmail.com>
2023-04-09 17:19:45 +0900

    Creating a Simple Image Viewer

---
+++ Project8_2/app/src/main/java/com/cookandroid/project8_2/MainActivity.java
@@ -0,0 +1,60 @@
+package com.cookandroid.project8_2;
+
+import java.io.File;
+
+import android.os.Environment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+// 2021041059 김나영
+public class MainActivity extends AppCompatActivity {
+    Button btnPrev, btnNext;
+    myPictureView myPicture;
+    int curNum=1;
+    File[] imageFiles;
+    String imageFname;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("간단 이미지 뷰어");
+        ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.WRITE_EXTERNAL_STORAGE},MODE_PRIVATE);
+
+        btnPrev = findViewById(R.id.btnPrev);
+        btnNext = findViewById(R.id.btnNext);
+        myPicture = findViewById(R.id.myPictureView1);
+
+        imageFiles = new File(Environment.getExternalStorageDirectory()
+                .getAbsolutePath()+"/Pictures").listFiles();
+
+        imageFname = imageFiles[curNum].toString();
+        myPicture.imagePath=imageFname;
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (curNum <= 1) {
+                    Toast.makeText(getApplicationContext(), "첫번째 그림입니다", Toast.LENGTH_SHORT).show();
+                } else {
+                    curNum--;
+                    imageFname=imageFiles[curNum].toString();
+                    myPicture.imagePath=imageFname;
+                    myPicture.invalidate();
+                }
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (curNum >= imageFiles.length - 1) {
+                    Toast.makeText(getApplicationContext(), "마지막 그림입니다", Toast.LENGTH_SHORT).show();
+                } else {
+                    curNum++;
+                }
+            }
+        });
+    }
+}
