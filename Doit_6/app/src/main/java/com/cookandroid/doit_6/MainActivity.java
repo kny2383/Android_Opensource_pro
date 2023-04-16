c5d24e51a0444ba41414c8d8d531d14f60c4012c
kny2383 <kny2383@gmail.com>
2023-04-16 20:49:54 +0900

    Showing the chic bar and progress bar

---
+++ Doit_6/app/src/main/java/com/cookandroid/doit_6/MainActivity.java
@@ -0,0 +1,42 @@
+package com.cookandroid.doit_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    SeekBar seekBar;
+    ProgressBar progressBar;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBar = findViewById(R.id.seekBar);
+        progressBar = findViewById(R.id.progressBar);
+        editText = findViewById(R.id.editTextTextPersonName);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                progressBar.setProgress(i);
+                editText.setText(String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
